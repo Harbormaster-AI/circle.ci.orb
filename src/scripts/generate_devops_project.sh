2dcd3097214d463d60163a22567b82f8221bb296
Steven Randolph <steven.randolph@realmethods.com>
2021-05-17 16:33:42 -0500

    Update generate_devops_project.sh

--- src/scripts/generate_devops_project.sh
+++ src/scripts/generate_devops_project.sh
@@ -1,4 +1,4 @@
 if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 echo Generating DevOps Project
-harbormaster project_generate ~/$CIRCLE_PROJECT_REPONAME/$HARBORMASTER_PROJECT_AS_CODE_FILE
+harbormaster project_generate ~/"$CIRCLE_PROJECT_REPONAME"/"$HARBORMASTER_PROJECT_AS_CODE_FILE"
