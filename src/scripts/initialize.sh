909a7d18302c4aef48afded250f415237000ed94
Steven Randolph <steven.randolph@realmethods.com>
2021-05-17 18:48:10 -0500

    format

--- src/scripts/initialize.sh
+++ src/scripts/initialize.sh
@@ -2,19 +2,19 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 echo print environmental variables just to check things out
 printenv
-          
+
 echo update npm
 npm install -g npm@latest
 
 echo install npm-run
-npm install -g npm-run@latest    
-          
+npm install -g npm-run@latest
+
 echo install Harbormaster command line interface
 npm install -g harbormaster-ai
-          
+
 echo initialize user for Harbormaster
 npm-run harbormaster init "${HARBORMASTER_API_TOKEN}"
-  
+
 echo update the apt-get package index
 apt-get update
 
