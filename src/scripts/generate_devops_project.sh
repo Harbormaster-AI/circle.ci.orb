31b2eddffba16acd6192b21467fb7d474c2a536f
Steven Randolph <steven.randolph@realmethods.com>
2021-05-18 15:20:52 -0500

    Delete generate_devops_project.sh

--- src/scripts/generate_devops_project.sh
+++
@@ -1,4 +0,0 @@
-if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
-
-echo Generating DevOps Project
-$SUDO harbormaster project_generate ~/"$CIRCLE_PROJECT_REPONAME"/"$HARBORMASTER_PROJECT_AS_CODE_YAML_FILE"
