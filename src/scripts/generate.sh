434c4fc343b31c8bd49593e7bbfb4ef539206a90
Steven Randolph <steven.randolph@realmethods.com>
2021-05-19 00:58:45 -0500

    consolidate initialization and generation

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -10,11 +10,15 @@ echo install Harbormaster command line interface
 $SUDO npm install -g harbormaster-ai
 
 echo initialize user for Harbormaster
-$SUDO npm-run harbormaster init "${USER_HARBORMASTER_API_TOKEN}"
+$SUDO npm-run harbormaster init "$USER_HARBORMASTER_API_TOKEN"
 
 echo update the apt-get package index
 $SUDO apt-get update
 
 echo install dos2unix to convert uploaded files
 $SUDO apt-get install -y dos2unix
+
+echo Generating DevOps Project
+$SUDO harbormaster project_generate ~/"$CIRCLE_PROJECT_REPONAME"/"$HARBORMASTER_PROJECT_AS_CODE_YAML_FILE"
+
 
