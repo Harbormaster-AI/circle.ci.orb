bf1130f7dabd0cd810c9c172be36ee564909874d
Steven Randolph <steven.randolph@realmethods.com>
2021-05-19 09:56:39 -0500

    removed quotes around env vars

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -19,4 +19,4 @@ echo install dos2unix to convert uploaded files
 $SUDO apt-get install -y dos2unix
 
 echo Generating DevOps Project
-$SUDO npm-run harbormaster project_generate ~/"$CIRCLE_PROJECT_REPONAME"/"$HARBORMASTER_PROJECT_AS_CODE_YAML_FILE"
+$SUDO npm-run harbormaster project_generate ~/$CIRCLE_PROJECT_REPONAME/$HARBORMASTER_PROJECT_AS_CODE_YAML_FILE
